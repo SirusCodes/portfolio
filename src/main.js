1a457bebbb308ee8594235cb444f9b113c911cbf
SirusCodes <darshandrander@gmail.com>
2022-06-13 01:54:48 +0530

    feat: add mobile view

--- src/main.js
+++ src/main.js
@@ -1,18 +1,25 @@
 const fader = document.querySelectorAll(".fade");
 
 const options = {
-    threshold: 0.5
+	threshold: 0.5
 };
 
 const appearOnScroll = new IntersectionObserver((entries, observer) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add("appear");
-            observer.unobserve(entry.target);
-        }
-    });
+	entries.forEach((entry) => {
+		if (entry.isIntersecting) {
+			entry.target.classList.add("appear");
+			observer.unobserve(entry.target);
+		}
+	});
 }, options);
 
 fader.forEach((fader) => {
-    appearOnScroll.observe(fader);
+	appearOnScroll.observe(fader);
+});
+
+const menu = document.querySelector(".menu");
+
+menu.addEventListener("click", (e) => {
+	const navbar = document.querySelector(".navbar-list");
+	navbar.classList.toggle("open");
 });
