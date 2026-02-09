a4c711020ed1ef8425aa098a9f923b5d6dc1a10c
SirusCodes <hey@darshanrander.com>
2026-02-09 23:25:47 +0530

    feat: integrate astro-umami for analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 import generatePDFFromTex from "./lib/integrations/generatePDFFromTex";
 
+import yeskunallumami from "@yeskunall/astro-umami";
+
 const isGHPages = process.env.IS_GH_PAGES ?? false;
 // https://astro.build/config
 export default defineConfig({
@@ -12,5 +14,14 @@ export default defineConfig({
 		? "https://beta.darshanrander.com"
 		: "https://darshanrander.com",
 
-	integrations: [preact(), sitemap(), robotsTxt(), generatePDFFromTex()]
+	integrations: [
+		preact(),
+		sitemap(),
+		robotsTxt(),
+		// generatePDFFromTex(),
+		yeskunallumami({
+			id: "a9e17105-6708-419e-bba4-32930be50670",
+			endpointUrl: "https://analytics.darshanrander.com"
+		})
+	]
 });
